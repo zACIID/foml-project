d72a2561037e56117b8ddf532c7731a3a82a55b8
zACIID <marchioropierluigi@gmail.com>
2023-12-16 02:47:06 +0100

    feat(constants): defined constants used in CocoDataset

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,1 +1,17 @@
+import os
+from typing import Tuple
+
+
 RND_SEED = 777
+
+ROOT_COCO_DIR = os.path.join("..", "..", "data")
+"""Directory where COCO dataset is stored/downloaded"""
+
+INPUT_IMAGE_SIZE: Tuple[int, int] = (224, 224)
+"""Input size of models in this project"""
+
+COCO_IMAGE_MEANS: Tuple[float, float, float] = (119.8632, 113.9443, 103.9443)
+"""Dataset, channel-wise means for MS COCO2017 dataset (training) images"""
+
+COCO_IMAGE_STDS: Tuple[float, float, float] = (59.4222, 58.1447, 59.0335)
+"""Dataset, channel-wise standard deviations for MS COCO2017 dataset (training) images"""
