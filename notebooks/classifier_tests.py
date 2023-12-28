b3209100f50f282de4821070d1f5387c7397891c
zACIID <marchioropierluigi@gmail.com>
2023-12-28 14:16:30 +0100

    chore(classifier_tests): updated test notebook

--- notebooks/classifier_tests.py
+++ notebooks/classifier_tests.py
@@ -20,9 +20,16 @@ import torch
 import torch.utils.data as data
 import src.datasets.custom_coco_dataset as coco
 import src.ensemble_training.ada_boost as ada
+from src.classifiers import alex_net as ax
 
 # %%
-dataset = coco.COCO_TEST_DATASET
+torch.cuda.is_available()
+
+# %%
+torch.version.cuda
+
+# %%
+dataset = coco.COCO_TRAIN_DATASET
 dataset.load()
 
 # %% [markdown]
@@ -31,20 +38,31 @@ dataset.load()
 # %%
 ada_boost = ada.AdaBoost(dataset=dataset, n_eras=1, n_classes=2)
 
-# %%
-strong_learner = ada_boost.start(verbose=4)
+# %% [raw]
+# strong_learner = ada_boost.start(verbose=4)
 
-# %%
-subset = data.Subset(dataset=dataset, indices=[x for x in range(48)])
-for batch in data.DataLoader(subset, 4, shuffle=True):
-    batch: coco.BatchType
-    ids, imgs, labels, weights = batch
-    preds = strong_learner.predict_image(imgs)
-    
-    print(f"Predictions:\n{preds}")
-    print(f"Actual:\n{labels}")
+# %% [raw]
+# subset = data.Subset(dataset=dataset, indices=[x for x in range(48)])
+# for batch in data.DataLoader(subset, 4, shuffle=True):
+#     batch: coco.BatchType
+#     ids, imgs, labels, weights = batch
+#     preds = strong_learner.predict_image(imgs)
+#     
+#     print(f"Predictions:\n{preds}")
+#     print(f"Actual:\n{labels}")
 
 # %% [markdown]
 # ## AlexNet Tests
+
+# %%
+alex_net = ax.AlexNet(
+    act_fun=torch.nn.SiLU
+)
+alex_net.fit(
+    dataset=dataset,
+    verbose=5,
+    learning_rate=0.1,
+    momentum=1
+)
 
 # %%
