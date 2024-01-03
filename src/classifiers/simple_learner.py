9c295ddf076485150121bb76625b56b6c16bf753
zACIID <marchioropierluigi@gmail.com>
2024-01-03 21:36:14 +0100

    fix(simple_learner): fixed wrongly referenced variable

--- src/classifiers/simple_learner.py
+++ src/classifiers/simple_learner.py
@@ -254,7 +254,7 @@ class SimpleLearner(nn.Module):
         a) a tensor of shape: (k_classes, 1) if the number of samples is 1
         b) a tensor of shape: (n_samples, k_classes) otherwise
         """
-        dim: int = 0 if batch.dim() == 3 else 1
+        dim: int = 0 if samples.dim() == 3 else 1
         with no_grad():
             self.eval()  # Set the model to evaluation mode (if applicable)
 
